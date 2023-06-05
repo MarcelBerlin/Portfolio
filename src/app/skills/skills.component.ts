f9b63bb190120bdfaed8112e7d250f63427567a3
Marcel Berlin <berlin_m@gmx.de>
2023-06-05 15:26:56 +0200

    integrated aos function

--- src/app/skills/skills.component.ts
+++ src/app/skills/skills.component.ts
@@ -1,10 +1,60 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-skills',
   templateUrl: './skills.component.html',
   styleUrls: ['./skills.component.scss']
 })
-export class SkillsComponent {
+export class SkillsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+
+  }
+
+  mySkills: any = [
+
+    {
+      name: 'Angular',
+      img: 'Angular.png'
+    },
+    {
+      name: 'Typescript',
+      img: 'Typescript.png'
+    },
+    {
+      name: 'JavaScript',
+      img: 'JavaScript.png'
+    },
+    {
+      name: 'HTML',
+      img: 'HTML.png'
+    },
+    {
+      name: 'Firebase',
+      img: 'Firebase.png'
+    },
+    {
+      name: 'Github',
+      img: 'Git.png'
+    },
+    {
+      name: 'CSS',
+      img: 'CSS.png'
+    },
+    {
+      name: 'Rest-Api',
+      img: 'Rest-Api.png'
+    },
+    {
+      name: 'Scrum',
+      img: 'Scrum.png'
+    },
+    {
+      name: 'Material Design',
+      img: 'MaterialDesign.png'
+    }
+  ];
 
 }
