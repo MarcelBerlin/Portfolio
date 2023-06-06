d6354d65ed43bc010c0c4cae5e4c17969d97c50a
Marcel Berlin <berlin_m@gmx.de>
2023-06-06 14:09:18 +0200

    portfolio section started

--- src/app/portfolio/portfolio.component.ts
+++ src/app/portfolio/portfolio.component.ts
@@ -1,10 +1,43 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-portfolio',
   templateUrl: './portfolio.component.html',
   styleUrls: ['./portfolio.component.scss']
 })
-export class PortfolioComponent {
+export class PortfolioComponent implements OnInit {
+
+  constructor() {}
+
+  ngOnInit(): void {
+    
+  }
+
+  projects: any = [
+    {
+      name: 'Join',
+      img: 'join.png', 
+      language: 'JavaScript | HTML | CSS | Git',
+      text: 'A group created Kanban-Board',
+      link: 'https://join.marcelberlin-developer.de',
+      linkGit: 'https://github.com/MarcelBerlin/JOIN--GroupWork.git'
+    },
+    {
+      name: 'Sharkie',
+      img: 'Sharkie.png', 
+      language: 'JavaScript | HTML | CSS',
+      text: 'A simple Jump-and-Run game based on an object-oriented approach. Help sharkie to find coins and poison bottles to fight against the killer whale.',
+      link: 'https://sharkie.marcelberlin-developer.de',
+      linkGit: 'https://github.com/MarcelBerlin/sharkie.git'
+    },
+    {
+      name: 'Pokedex',
+      img: 'pokedex-detail.png', 
+      language: 'JavaScript | HTML | CSS | Api',
+      text: 'Based on PokéAPI a simple library for some information about Pokemons.',
+      link: 'https://pokedex.marcelberlin-developer.de',
+      linkGit: 'https://github.com/MarcelBerlin/Pokedex.git'
+    }
+  ];
 
 }
