c4136bbe22aa32d6f43e5f7986e222414ba8968d
Marcel Berlin <berlin_m@gmx.de>
2023-06-07 11:24:44 +0200

    portfolio updated

--- src/app/portfolio/portfolio.component.ts
+++ src/app/portfolio/portfolio.component.ts
@@ -19,7 +19,7 @@ export class PortfolioComponent implements OnInit {
       img: 'join.png', 
       language: 'JavaScript | HTML | CSS | Git',
       text: 'A group created Kanban-Board',
-      link: 'https://join.marcelberlin-developer.de',
+      link: 'https://join.marcelberlin-developer.de/',
       linkGit: 'https://github.com/MarcelBerlin/JOIN--GroupWork.git'
     },
     {
@@ -27,7 +27,7 @@ export class PortfolioComponent implements OnInit {
       img: 'Sharkie.png', 
       language: 'JavaScript | HTML | CSS',
       text: 'A simple Jump-and-Run game based on an object-oriented approach. Help sharkie to find coins and poison bottles to fight against the killer whale.',
-      link: 'https://sharkie.marcelberlin-developer.de',
+      link: 'https://sharkie.marcelberlin-developer.de/',
       linkGit: 'https://github.com/MarcelBerlin/sharkie.git'
     },
     {
@@ -35,9 +35,18 @@ export class PortfolioComponent implements OnInit {
       img: 'pokedex-detail.png', 
       language: 'JavaScript | HTML | CSS | Api',
       text: 'Based on PokéAPI a simple library for some information about Pokemons.',
-      link: 'https://pokedex.marcelberlin-developer.de',
+      link: 'https://pokedex.marcelberlin-developer.de/',
       linkGit: 'https://github.com/MarcelBerlin/Pokedex.git'
-    }
+    },
+    {
+      name: 'Ring-of-fire',
+      img: 'pokedex-detail.png', 
+      language: 'Angular | Firebase',
+      text: 'A simple Card created with Angular and Firebase.',
+      link: 'https://ringoffire.marcelberlin-developer.de/',
+      linkGit: 'https://github.com/MarcelBerlin/Ring-of-Fire.git'
+    },
+
   ];
 
 }
